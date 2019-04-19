58c5fe36e264aa96f539eac0a49df3137ae80951
Phillip Gray <phillipgray@Phillips-iMac.local>
2019-04-19 10:27:53 -0400

    refactor for better unit testing

--- index.js
+++ index.js
@@ -1,122 +1,43 @@
-const path = require('path');
-const fs = require('fs-extra');
-const { xml2js } = require('xml-js');
+const errorHandler = require('./lib/errorHandler');
+const parseOpf = require('./lib/parseOpf');
+const coverFromManifest = require('./lib/coverFromManifest.js');
+const coverFromMeta = require('./lib/coverFromMeta.js');
+const opfHrefToPath = require('./lib/opfHrefToPath.js');
 
 /* export a function that returns an error or a string with the path to the cover */
-module.exports = (pathToEpub, cb) =>
+module.exports = (pathToEpub, cb = null) =>
   new Promise(async (resolve, reject) => {
-    /*= ============================================
-=            handleError            =
-============================================= */
-    /* establish an error handler for if anything goes wrong */
+    /* a helper that will pass an error message to either the callback or Promise.reject */
+    const handleError = errorHandler(reject, cb); // returns a function
 
-    function handleError(message) {
-      const err = new Error(message);
-      /* if there's a callback, call it with the err obj and data as null */
-      if (cb) {
-        return cb(err, null);
-      }
-
-      /* reject the promise with the error object */
-      return reject(err);
-    }
-
-    /*= ====  End of handleError  ====== */
-
-    /*= ============================================
-    =            resolve            =
-    ============================================= */
-
-    /* try to find META-INF/container.xml */
-
-    let containerXml;
-    let relativeOpfPath;
-    let opfXml;
-    let cover;
-
+    /* get the path to the opf and convert opf to js */
+    let opfData;
     try {
-      containerXml = await fs.readFile(
-        path.resolve(pathToEpub, `META-INF/container.xml`),
-        'utf8'
-      );
+      opfData = await parseOpf(pathToEpub);
     } catch (e) {
-      return handleError(
-        `Could not read META-INF/container.xml. Make sure the EPUB is unzipped.`
-      );
+      return handleError(e.message);
     }
+    const { opfPath, opfJs } = opfData;
 
-    /* try to get the path to opf */
-    const containerJs = xml2js(containerXml, { compact: true });
+    /* check the manifest for item with property `cover-image`; fallback to checking the meta tag with name `cover` */
+    const cover = coverFromManifest(opfJs) || coverFromMeta(opfJs);
 
-    try {
-      relativeOpfPath =
-        containerJs.container.rootfiles.rootfile._attributes['full-path'];
-    } catch (e) {
-      return handleError(
-        `Could not get the path to the rootfile from META-INF/container.xml.`
-      );
-    }
-
-    const opfPath = path.resolve(pathToEpub, relativeOpfPath);
-
-    /* try to read the opf */
-
-    try {
-      opfXml = await fs.readFile(opfPath, 'utf8').catch(handleError);
-    } catch (e) {
-      return handleError(`Could not find the rootfile at ${pathToEpub}.`);
-    }
-
-    /* try to parse the xml  */
-
-    const opfJs = xml2js(opfXml, { compact: true });
-
-    /* try to find the item with the id cover-image and get its href value */
-
-    /* check the manifest for item with property 'cover-image'; fallback to checking the meta tag with name 'cover' */
-
-    const {
-      package: {
-        manifest,
-        metadata: { meta },
-      },
-    } = opfJs;
-
-    const [manifestCoverItem] = manifest.item.filter(
-      item => item._attributes.properties === 'cover-image'
-    );
-    const [metaDataCoverReference] = meta.filter(
-      item => item._attributes.name === 'cover'
-    );
-
-    if (manifestCoverItem) {
-      cover = manifestCoverItem._attributes.href;
-    } else if (metaDataCoverReference) {
-      const coverId = metaDataCoverReference._attributes.content;
-      const [metaCoverItem] = manifest.item.filter(
-        item => item._attributes.id === coverId
-      );
-      if (!metaCoverItem) {
-        return handleError(
-          'No cover image specified in package metadata or package manifest item properties.'
-        );
-      }
-      cover = metaCoverItem._attributes.href;
-    } else {
+    /* if there is no cover, handle error */
+    if (!cover) {
       return handleError(
         'No cover image specified in package metadata or package manifest item properties.'
       );
     }
 
     /* create an absolute path from the relative coverImage path and the opf parent directory path */
-    const coverImagePath = path.resolve(path.dirname(opfPath), cover);
+    const coverImagePath = opfHrefToPath(opfPath, cover);
 
-    /* if there's a callback, call it passing null for the err and the absolute path to the coverImage as the value for data */
+    /* if there's a callback, call it passing null for the err and the absolute path to the cover image as the value for data */
     if (cb) {
       return cb(null, coverImagePath);
     }
 
-    /* resolve the promise with the absolute path to the coverImage */
+    /* resolve the promise with the absolute path to the cover image */
     return resolve(coverImagePath);
   });
 
